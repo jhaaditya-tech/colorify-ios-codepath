6025f426944b3293f9077649a63d1327d08fd310
Aditya Jha <adityajha.swe@gmail.com>
2024-12-18 20:21:28 -0600

    Initial Commit

---
+++ Colorify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Colorify
+//
+//  Created by Aditya Jha on 12/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ColorifyTests/ColorifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColorifyTests.swift
+//  ColorifyTests
+//
+//  Created by Aditya Jha on 12/18/24.
+//
+
+import Testing
+@testable import Colorify
+
+struct ColorifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
